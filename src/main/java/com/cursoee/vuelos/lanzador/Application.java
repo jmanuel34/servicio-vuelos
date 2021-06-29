4b1214ec011e342e1f713625ffd4c94c8c0fbfff
jmanuel34 <jmanuel34@gmail.com>
2021-06-29 18:59:59 +0200

    configuracion

--- src/main/java/com/cursoee/vuelos/lanzador/Application.java
+++ src/main/java/com/cursoee/vuelos/lanzador/Application.java
@@ -3,7 +3,10 @@ package com.cursoee.vuelos.lanzador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @EnableJpaRepositories(basePackages = "com.cursoee.vuelos.dao")
 @EntityScan(basePackages = "com.cursoee.vuelos.model")
@@ -16,5 +19,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
